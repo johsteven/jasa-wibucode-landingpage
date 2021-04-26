71b80ad47b2e049876dbd257465d5611a885bae6
johsteven <johsteven222004@gmail.com>
2021-04-26 09:18:46 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// Variables
+
+// Functions
+
+// Main
+window.addEventListener("load", init);
+
+function init() {
+    load();
+    eventListener();
+}
+
+function load() {
+
+}
+
+function eventListener() {}
